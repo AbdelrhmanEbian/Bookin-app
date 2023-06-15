0529dca9829c638bbe9d28c0bd1f95271cb8c03d
AbdelrhmanEbian <116501838+AbdelrhmanEbian@users.noreply.github.com>
2023-06-15 19:53:39 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ app.use(uploader())
 app.use(express.json())
 app.use('/uploads',express.static(__dirname+'/uploads'))
 const corsOptions = {
-
+    orign:'*',
     credentials: true, // Allow sending credentials
   }
   app.use(cors(corsOptions))
